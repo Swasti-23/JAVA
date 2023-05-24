7dabf7ab197d625cf6c5efc474f9280230fa70fd
Swasti-23 <117920342+Swasti-23@users.noreply.github.com>
2023-05-24 12:47:58 +0530

    Ceiling of a number using binary search
    return the number if present or return the number just greater than it from the array

--- DSA/Q1.java
+++ DSA/Q1.java
@@ -8,6 +8,9 @@ public class Q1 {
         System.out.println(ans);
     }
     static int binarySearch(int[] arr,int target){
+        if(target>arr[arr.length-1]){
+            return -1;
+        }
         int start=0;
         int end=arr.length-1;
         while(start<=end){
